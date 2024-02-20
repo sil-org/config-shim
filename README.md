cdd5b10c0c753a01e361a728de429081cffa1867
briskt <3172830+briskt@users.noreply.github.com>
2024-02-20 07:28:42 -0700

    first prototype

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# template-public
-general template for public repositories
+# config-shim
+
+Languages like PHP, when used in a web application environment are not well-suited for application configuration tools like AWS AppConfig. The entire application is initialized for every web request. This means that without persistent data storage, configuration must be fetched from the API on every request. This tool, provides for a quick migration from an environment-based configuration to AppConfig by reading from the config API once at the time the server starts. It passes configuration into environment variables in the process that starts your web server.
+
+## Configuration
+
+In your startup script insert a call to `config-shim` like this:
+
+```shell
+config-shim --app my_app --config default --env prod apache2ctl -D FOREGROUND
+```
+
+## Parameters
+
+config-shim command-line parameter format is like `config-shim <flags> <command>`
+
+### Flags
+- `--app`: Application Identifier, can be the name of the application or the ID assigned by AWS
+- `--config`: Configuration Profile Identifier, can be the profile name or the ID assigned by AWS
+- `--env`: Environment Identifier, can be the name of the environment or the ID assigned by AWS
+
+### Command
+All parameters after the last flag are used as the command to execute after loading the environment variables from the config data received from AppConfig.
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/appconfigdata"
+)
+
+var verbose bool
+
+type AppConfigParams struct {
+	applicationID   string
+	environmentID   string
+	configProfileID string
+}
+
+func main() {
+	params := readFlags()
+
+	vars, err := getConfig(params)
+	if err != nil {
+		log.Fatalf("failed to get config: %s", err)
+	}
+
+	args := flag.Args()
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = append(os.Environ(), vars...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if verbose {
+		log.Printf("running %q with args: %s and env: %s", args[0], args[1:], cmd.Env)
+	}
+	if err = cmd.Run(); err != nil {
+		os.Exit(2)
+	}
+}
+
+func readFlags() (params AppConfigParams) {
+	flag.StringVar(&params.applicationID, "app", "", "application identifier")
+	flag.StringVar(&params.environmentID, "env", "", "environment identifier")
+	flag.StringVar(&params.configProfileID, "config", "", "config profile identifier")
+	flag.BoolVar(&verbose, "v", false, "verbose output")
+	flag.Parse()
+
+	if params.applicationID == "" {
+		log.Printf("specify application identifier with --app flag")
+		os.Exit(1)
+	}
+	if params.environmentID == "" {
+		log.Printf("specify environment identifier with --env flag")
+		os.Exit(1)
+	}
+	if params.configProfileID == "" {
+		log.Printf("specify config profile identifier with --config flag")
+		os.Exit(1)
+	}
+
+	if flag.Arg(0) == "" {
+		log.Printf("must specify program to execute")
+		os.Exit(1)
+	}
+
+	if verbose {
+		log.Printf("reading from app %q, env %q, profile %q",
+			params.applicationID, params.environmentID, params.configProfileID)
+	}
+
+	return
+}
+
+func getConfig(params AppConfigParams) ([]string, error) {
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	client := appconfigdata.NewFromConfig(cfg)
+	session, err := client.StartConfigurationSession(ctx, &appconfigdata.StartConfigurationSessionInput{
+		ApplicationIdentifier:          &params.applicationID,
+		ConfigurationProfileIdentifier: &params.configProfileID,
+		EnvironmentIdentifier:          &params.environmentID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	configuration, err := client.GetLatestConfiguration(ctx, &appconfigdata.GetLatestConfigurationInput{
+		ConfigurationToken: session.InitialConfigurationToken,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	vars := strings.Split(string(configuration.Configuration), "\n")
+
+	if verbose {
+		log.Printf("read %d lines", len(vars))
+		log.Printf("vars: %s", vars)
+	}
+
+	return vars, nil
+}
